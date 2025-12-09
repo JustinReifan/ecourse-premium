cb935d264748bea95b3606d86913a54271a70928
justin <justin@gmail.com>
2025-12-09 13:54:14 +0700

    logo

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -1,15 +1,6 @@
 export default function AppLogo() {
     return (
         <a href="/" className="flex items-center gap-2">
-<<<<<<< HEAD
-            <div className="text-sidebar-primary-foreground flex aspect-square size-10 flex-shrink-0 items-center justify-center rounded-md">
-                <img src="/storage/landing/logo/logo.png" alt="Logo" className="w-full object-contain" /* object-contain menjaga rasio gambar */ />
-            </div>
-
-            <div className="text-sidebar-primary-foreground flex-1 text-left text-sm leading-none font-semibold md:text-base">
-                <span className="truncate">Grow Up Muslimpreneur</span>
-            </div>
-=======
             <div className="text-sidebar-primary-foreground flex aspect-square size-20 flex-shrink-0 items-center justify-center rounded-md md:size-25">
                 <img src="/storage/landing/logo/nav-logo.png" alt="Logo" className="w-full object-contain" />
             </div>
@@ -17,7 +8,6 @@ export default function AppLogo() {
             {/* <div className="text-sidebar-primary-foreground flex-1 text-left text-sm leading-none font-semibold md:text-base">
                 <span className="truncate">Grow Up Muslimpreneur</span>
             </div> */}
->>>>>>> 3faaec801ba1342ca0378c5f53cd8908ebfafd4d
         </a>
     );
 }
